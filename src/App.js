64f75dbe4a64ed7bcb6fedcd6a67daac9027863a
Sunflo-H <sunflo0524@gmail.com>
2025-05-02 00:32:56 +0900

    첫 렌더링 때부터 데이터를 읽은채로 시작

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
 import "./App.css";
 import ExcelReader from "./components/ExcelReader";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { child, get, getDatabase, ref, set } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBbkCm8B70Qi2aPMuL-YKZEZc7_ieAqwH0",
-  authDomain: "nordisk-management.firebaseapp.com",
-  projectId: "nordisk-management",
-  storageBucket: "nordisk-management.firebasestorage.app",
-  messagingSenderId: "601807263512",
-  appId: "1:601807263512:web:1ae00c6b549c67fe8c706d",
-  measurementId: "G-C7R3BKE0LD",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-
-function writeUserData(userId, name, email, imageUrl) {
-  set(ref(db, "users/" + userId), {
-    username: name,
-    email: email,
-    profile_picture: imageUrl,
-  });
-}
-
-const dbRef = ref(db);
-function readData() {
-  get(child(dbRef, `users/1`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
 
 function App() {
   return (
     <div className="App">
       <ExcelReader />
-      <div onClick={() => writeUserData(1, "홍길동", "gd@google.com", "")}>
-        쓰기
-      </div>
-      <div onClick={readData}>읽기</div>
     </div>
   );
 }
--- src/components/ExcelReader.jsx
+++ src/components/ExcelReader.jsx
@@ -1,9 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
 import "../App.css";
+import { child, get, getDatabase, ref, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
+const columnOrder = [
+  "상품코드",
+  "상품명",
+  "칼라",
+  "수량",
+  "00",
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbkCm8B70Qi2aPMuL-YKZEZc7_ieAqwH0",
+  authDomain: "nordisk-management.firebaseapp.com",
+  projectId: "nordisk-management",
+  storageBucket: "nordisk-management.firebasestorage.app",
+  messagingSenderId: "601807263512",
+  appId: "1:601807263512:web:1ae00c6b549c67fe8c706d",
+  measurementId: "G-C7R3BKE0LD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+function writeExcelData(excelData) {
+  excelData.forEach((data) => {
+    set(ref(db, "product/" + data.상품코드), {
+      상품코드: data.상품코드,
+      상품명: data.상품명,
+      칼라: data.칼라,
+      수량: data.수량,
+      "00": data["00"],
+      "01": data["01"],
+      "02": data["02"],
+      "03": data["03"],
+      "04": data["04"],
+      "05": data["05"],
+      "06": data["06"],
+      "07": data["07"],
+      "08": data["08"],
+      "09": data["09"],
+      10: data["10"],
+      11: data["11"],
+      12: data["12"],
+      13: data["13"],
+      14: data["14"],
+      15: data["15"],
+      16: data["16"],
+      17: data["17"],
+      18: data["18"],
+      19: data["19"],
+    });
+  });
+}
+
+const dbRef = ref(db);
 
 const ExcelReader = () => {
-  const [data, setData] = useState([]);
+  const [productsData, setProductsData] = useState([]);
   const [blinkedRow, setBlinkedRow] = useState(null);
 
   const handleFileUpload = (e) => {
@@ -16,7 +92,8 @@ const ExcelReader = () => {
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
       const jsonData = XLSX.utils.sheet_to_json(sheet);
-      setData(jsonData);
+      setProductsData(jsonData);
+      writeExcelData(jsonData);
     };
 
     reader.readAsBinaryString(file);
@@ -27,32 +104,30 @@ const ExcelReader = () => {
     setTimeout(() => setBlinkedRow(null), 60000); // 0.6초 뒤 초기화
   };
 
-  const columnOrder = [
-    "상품코드",
-    "상품명",
-    "칼라",
-    "수량",
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-  ];
+  function readData() {
+    get(child(dbRef, `product`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          const productsData = Object.entries(snapshot.val()).map(
+            ([상품코드, item]) => ({
+              상품코드,
+              ...item,
+            })
+          );
+          setProductsData(productsData);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    readData();
+  }, []);
 
   return (
     <div>
@@ -66,7 +141,7 @@ const ExcelReader = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, i) => (
+          {productsData.map((row, i) => (
             <tr
               key={i}
               onClick={() => handleRowClick(i)}
@@ -80,6 +155,9 @@ const ExcelReader = () => {
           ))}
         </tbody>
       </table>
+
+      <div onClick={() => writeExcelData()}>쓰기</div>
+      <div onClick={readData}>읽기</div>
     </div>
   );
 };
